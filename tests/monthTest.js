dca29f3a1241995d354ac053ed9d56c9647fab23
atocchi <andrewtocchi@gmail.c>
2022-12-17 01:49:16 -0800

    added better tests using tap, teardown now correctly closes redis session

--- tests/monthTest.js
+++ tests/monthTest.js
@@ -1,12 +1,39 @@
-const { test } = require('tap')
-const build = require('../app')
+const { test } = require('tap');
+const build = require('../app');
+const moment = require("moment");
 
-test('requests the "/" route', async t => {
+test('requests the "/v1/month" route', async t => {
+  t.plan(4)
   const app = build()
-
+ 
+  t.teardown(() => app.close())
+  
   const response = await app.inject({
     method: 'GET',
-    url: '/v1/month/2015-10'
+    url: '/v1/month/2017-04'
   })
   t.equal(response.statusCode, 200, 'returns a status code of 200')
+
+  let date = moment().format('YYYY-MM');
+  let future = moment(date, 'YYYY-MM').add(1,'M').format("YYYY-MM");
+
+  const response2 = await app.inject({
+    method: 'GET',
+    url: `/v1/month/${date}`
+  })
+  t.equal(response2.statusCode, 200, 'returns a status code of 200, for current month')
+
+  const response3 = await app.inject({
+    method: 'GET',
+    url: `/v1/month/${future}`
+  })
+  t.equal(response3.statusCode, 500, 'returns a status code of 500, for future month')
+
+  const response4 = await app.inject({
+    method: 'GET',
+    url: `/v1/month/hotdogs`
+  })
+  t.equal(response4.statusCode, 400, 'returns a status code of 400, for gibberish')
+
+
 })
--- tests/weekTest.js
+++ tests/weekTest.js
@@ -1,14 +1,38 @@
-const build = require('../app')
+const { test } = require('tap');
+const build = require('../app');
+const moment = require("moment");
 
-const test = async () => {
+
+test('requests the "/v1/week" route', async t => {
+  t.plan(4)
   const app = build()
+ 
+  t.teardown(() => app.close())
 
   const response = await app.inject({
     method: 'GET',
-    url: '/v1/month/2015'
+    url: '/v1/week/2018-11-11'
   })
+  t.equal(response.statusCode, 200, 'returns a status code of 200')
 
-  console.log('status code: ', response.statusCode)
-  console.log('body: ', response.body)
-}
-test()
+  let date = moment().format('YYYY-MM-DD');
+  let future = moment(date, 'YYYY-MM-DD').add(14,'d').format("YYYY-MM-DD");
+
+  const response2 = await app.inject({
+    method: 'GET',
+    url: `/v1/week/${date}`
+  })
+  t.equal(response2.statusCode, 200, 'returns a status code of 200, for current week')
+
+  const response3 = await app.inject({
+    method: 'GET',
+    url: `/v1/week/${future}`
+  })
+  t.equal(response3.statusCode, 500, 'returns a status code of 500, for two weeks in the future')
+
+  const response4 = await app.inject({
+    method: 'GET',
+    url: `/v1/week/hamburgers`
+  })
+  t.equal(response4.statusCode, 400, 'returns a status code of 400, for gibberish')
+})
