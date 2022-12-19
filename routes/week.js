a9fe3ea822c1adaf3629f7437d34c1440f49d73e
atocchi <andrewtocchi@gmail.c>
2022-12-18 22:14:10 -0800

    added else statement to save time from loading redis, added optional line for sorting by views

--- routes/week.js
+++ routes/week.js
@@ -9,74 +9,78 @@ module.exports = function (fastify, opts, done) {
       if(!valid){
         reply.header('Cache-Control', 'no-store').code(400).send({message: "MALFORMED DATE: SHOULD BE YYYY-MM-DD"});
       }
-      //LOAD REDIS TO GRAB CACHE IF EXISTS
-      const { redis } = fastify;
-      const getAsync = promisify(redis.get).bind(redis);
-      //GET START OF WEEK IN ORDER TO CALCULATE WHOLE WEEK
-      let day = moment(request.params.date, "YYYY-MM-DD").day()
-      let firstDay = moment(request.params.date, "YYYY-MM-DD").subtract(day, 'd').format("YYYY/MM/DD");
-      //TRY CATCH BLOCK IN-CASE REDIS FAILS WILL DEFAULT TO BATCH API CALLS
-      let cache = '';
-      try{
-        cache = await getAsync(firstDay);
-      }
-      catch(err){
-        console.log(err);
-      }
-      if(cache){
-        console.log('FOUND HASH');
-        let parse = JSON.parse(cache);
-        return parse.hashArr;
-      }
       else{
-        let hash = {};
-        let funcArr = [];
-        let dateArr = [];
-        let hashArr = [];
-        let rejectFlag = false;
-        //CREATE ARRAY OF AXIOS CALLS TO BATCH
-        for(let i =0; i < 7; i++){
-            let date = moment(firstDay, "YYYY/MM/DD").add(i, 'd').format("YYYY/MM/DD");
-            dateArr.push(date);
-            funcArr.push(axios(`https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access/${date}`));
+        //LOAD REDIS TO GRAB CACHE IF EXISTS
+        const { redis } = fastify;
+        const getAsync = promisify(redis.get).bind(redis);
+        //GET START OF WEEK IN ORDER TO CALCULATE WHOLE WEEK
+        let day = moment(request.params.date, "YYYY-MM-DD").day()
+        let firstDay = moment(request.params.date, "YYYY-MM-DD").subtract(day, 'd').format("YYYY/MM/DD");
+        //TRY CATCH BLOCK IN-CASE REDIS FAILS WILL DEFAULT TO BATCH API CALLS
+        let cache = '';
+        try{
+          cache = await getAsync(firstDay);
         }
-        data = await Promise.allSettled(funcArr);
-        for(i = 0; i < data.length; i++){
-          let newMap = data[i];
-          if(newMap.status === 'fulfilled'){
-            newMap.value.data.items[0].articles.map( item => {
-              if(hash[item.article] === undefined){ 
-                hash[item.article] = {articleName: item.article, views: item.views, most: item.views, date: dateArr[i]};
-              }
-              else{
-                if(hash[item.article].most < item.views){
-                  hash[item.article] = {articleName: item.article, views: hash[item.article].views + item.views, most : item.views, date: dateArr[i]};
-                }
-                else{
-                  hash[item.article] = {articleName: item.article, views: hash[item.article].views + item.views, most : hash[item.article].most, date: hash[item.article].date};
-                }
-              }
-            })
-          }
-          else{
-            rejectFlag = true;
-          }
+        catch(err){
+          console.log(err);
         }
-        //ITERATE THROUGH HASH TO MAKE IT LESS DEEP
-        hashArr = Object.values(hash);
-        //SAVE HASH TO REDIS FOR CACHE
-        if(!rejectFlag){
-          console.log('SAVING HASH')  
-          let str = JSON.stringify({hashArr}) 
-          if(str !== null){
-            redis.set(firstDay, str)
-          }
-        }
-        if(hashArr.length > 0){
-          return hashArr;
+        if(cache){
+          console.log('FOUND HASH');
+          let parse = JSON.parse(cache);
+          return parse.hashArr;
         }
         else{
-          reply.header('Cache-Control', 'no-store').code(500).send({message: "ERROR PULLING FROM WIKIAPI, POSSIBLE INVALID DATE; DATES MIGHT NOT BE IN WIKIPEDIA API YET"});
+          let hash = {};
+          let funcArr = [];
+          let dateArr = [];
+          let hashArr = [];
+          let rejectFlag = false;
+          //CREATE ARRAY OF AXIOS CALLS TO BATCH
+          for(let i =0; i < 7; i++){
+              let date = moment(firstDay, "YYYY/MM/DD").add(i, 'd').format("YYYY/MM/DD");
+              dateArr.push(date);
+              funcArr.push(axios(`https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access/${date}`));
+          }
+          data = await Promise.allSettled(funcArr);
+          for(i = 0; i < data.length; i++){
+            let newMap = data[i];
+            if(newMap.status === 'fulfilled'){
+              newMap.value.data.items[0].articles.map( item => {
+                if(hash[item.article] === undefined){ 
+                  hash[item.article] = {articleName: item.article, views: item.views, most: item.views, date: dateArr[i]};
+                }
+                else{
+                  if(hash[item.article].most < item.views){
+                    hash[item.article] = {articleName: item.article, views: hash[item.article].views + item.views, most : item.views, date: dateArr[i]};
+                  }
+                  else{
+                    hash[item.article] = {articleName: item.article, views: hash[item.article].views + item.views, most : hash[item.article].most, date: hash[item.article].date};
+                  }
+                }
+              })
+            }
+            else{
+              rejectFlag = true;
+            }
+          }
+          //ITERATE THROUGH HASH TO MAKE IT LESS DEEP
+          hashArr = Object.values(hash);
+          //SAVE HASH TO REDIS FOR CACHE
+          if(!rejectFlag){
+            console.log('SAVING HASH')  
+            let str = JSON.stringify({hashArr}) 
+            if(str !== null){
+              redis.set(firstDay, str)
+            }
+          } 
+          if(hashArr.length > 0){
+            // ADD THIS LINE TO SORT ARRAY
+            // hashArr.sort((a, b) => b.views - a.views);
+            return hashArr;
+          }
+          else{
+            reply.header('Cache-Control', 'no-store').code(500).send({message: "ERROR PULLING FROM WIKIAPI, POSSIBLE INVALID DATE; DATES MIGHT NOT BE IN WIKIPEDIA API YET"});
+          }
         }
       }
     })    
